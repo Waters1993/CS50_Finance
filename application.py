3e2c303cd2c9f294490bd89555ceee868c3d7d09
James Waters <jwaters1993@outlook.com>
2023-02-04 11:07:22 -0500

    postgres db

--- application.py
+++ application.py
@@ -4,7 +4,6 @@ import psycopg2
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-from mysql.connector.abstracts import MySQLCursorAbstract
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
